e9d534698cc8a68b39ccb7bfe3b32fbf8378780b
Liran Lavie <liranlavie1@gmail.com>
2022-08-07 05:53:14 +0300

    Adding remote bakcend

--- main.tf
+++ main.tf
@@ -56,20 +56,3 @@ module "vault" {
 
 
 
-
-
-#Create a storage account to store state
-#resource "azurerm_storage_account" "webserver_storage_account" {
-#  name                     = "tfstat001"
-#  resource_group_name      = azurerm_resource_group.terraform.name
-#  location                 = azurerm_resource_group.terraform.location
-#  account_tier             = "Standard"
-#  account_replication_type = "LRS"
-#}
-#
-##Add a storage container for storing state
-#resource "azurerm_storage_container" "tfstate" {
-#  name                  = "tfstate"
-#  storage_account_name  = azurerm_storage_account.webserver_storage_account.name
-#  container_access_type = "blob"
-#}
--- providors.tf
+++ providors.tf
@@ -5,15 +5,15 @@ terraform {
       version = "3.16.0"
     }
   }
-#    backend "azurerm" {
-#    resource_group_name  = "terraform_resource_group"
-#    storage_account_name = "terraform0111"
-#    container_name       = "tfstate"
-#    key                  = "terraform.tfstate"
-#  # key delete for safety
-#    access_key           = ""
-#
-#  }
+    backend "azurerm" {
+    resource_group_name  = "shared-resources"
+    storage_account_name = "tfstat001"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  # key delete for safety
+    access_key           = ""
+
+  }
 
   required_version = ">= 0.14"
 }
